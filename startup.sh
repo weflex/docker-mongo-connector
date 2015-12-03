8b440a1d1d0c52a343325963cffb45271e8a7c39
yorkie <yorkiefixer@gmail.com>
2015-12-03 12:03:01 +0800

    use env

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "rs.isMaster()" > is_master_check
-is_master_result=`mongo --host mongo< is_master_check`
+is_master_result=`mongo --host ${MONGO}< is_master_check`
 
 expected_result="\"ismaster\" : true"
 
@@ -10,7 +10,7 @@ do
   if [ "${is_master_result/$expected_result}" = "$is_master_result" ] ; then
     echo "Waiting for Mongod node to assume primary status..."
     sleep 3
-    is_master_result=`mongo --host mongo< is_master_check`
+    is_master_result=`mongo --host ${MONGO}< is_master_check`
   else
     echo "Mongod node is now primary"
     break;
