ec47fd1ef41f6c5f116f86d540d742889940d503
yorkie <yorkiefixer@gmail.com>
2015-12-04 15:13:12 +0800

    add continue-on-error flag

--- startup.sh
+++ startup.sh
@@ -29,13 +29,13 @@ do
     break
   fi
 done
-
 sleep 1
 
 mongo="${MONGO:-mongo}"
 elasticsearch="${ELASTICSEARCH:-elasticsearch}"
 
 mongo-connector --auto-commit-interval=0 \
+  --continue-on-error
   --oplog-ts=/data/oplog.ts \
   --main ${mongo}:27017 \
   --target-url ${elasticsearch}:9200 \
